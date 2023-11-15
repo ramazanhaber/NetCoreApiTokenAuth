341b2b72b3669fceab7930d84fb1ccc8fa0c77b5
RAMBO3 <ramazanhaberr@gmail.com>
2023-11-15 14:19:25 +0300

    1

--- NetCoreApiTokenAuth/Program.cs
+++ NetCoreApiTokenAuth/Program.cs
@@ -1,18 +1,13 @@
-
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddCors();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -26,17 +21,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-
-
 var app = builder.Build();
 
-
-//app.UseCors(builder => builder
-//.AllowAnyHeader()
-//.AllowAnyMethod()
-//.SetIsOriginAllowed((host) => true)
-//.AllowCredentials()
-//);
 app.UseCors(builder => builder
 .AllowAnyHeader()
 .AllowAnyMethod()
