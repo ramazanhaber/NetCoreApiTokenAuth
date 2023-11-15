c9b6e08f5fc351f69aa2ac7bcc185ea645131eb9
RAMBO3 <ramazanhaberr@gmail.com>
2023-11-15 14:14:43 +0300

    Add project files.

---
+++ NetCoreApiTokenAuth/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NetCoreApiTokenAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        // NuGet\Install-Package Microsoft.AspNetCore.Authentication.JwtBearer -Version 7.0.14
+
+        [Route("get")]
+        [Authorize]
+        [HttpGet]
+        public string Get()
+        {
+            return "RAMBO";
+        }
+
+        [Route("loginol")]
+        [HttpGet]
+        public string login(string username)
+        {
+            return GenerateToken(username);
+        }
+
+      
+        private string GenerateToken(string username)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-key"));
+
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var tokeOptions = new JwtSecurityToken(
+                claims: new List<Claim>(),
+                expires: DateTime.Now.AddHours(10),
+                signingCredentials: signinCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+
+            return tokenString;
+
+        }
+
+    }
+}
---
+++ NetCoreApiTokenAuth/Program.cs
@@ -0,0 +1,63 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-keyyour-secret-key"))
+        };
+    });
+
+
+
+var app = builder.Build();
+
+
+//app.UseCors(builder => builder
+//.AllowAnyHeader()
+//.AllowAnyMethod()
+//.SetIsOriginAllowed((host) => true)
+//.AllowCredentials()
+//);
+app.UseCors(builder => builder
+.AllowAnyHeader()
+.AllowAnyMethod()
+.AllowAnyOrigin()
+);
+
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "myapi v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();// auth için
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
