47ebe8e790030653a6d2c8de9b7784c6028358a2
RAMBO3 <ramazanhaberr@gmail.com>
2023-12-20 14:57:03 +0300

    11

--- NetCoreApiTokenAuth/Controllers/CachController.cs
+++ NetCoreApiTokenAuth/Controllers/CachController.cs
@@ -1,13 +1,5 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.IdentityModel.Tokens;
-using NetCoreApiTokenAuth.Models;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-
 namespace NetCoreApiTokenAuth.Controllers
 {
     [ApiController]
@@ -15,13 +7,10 @@ namespace NetCoreApiTokenAuth.Controllers
     public class CachController : ControllerBase
     {
         private readonly IMemoryCache _cache;
-
         public CachController(IMemoryCache memoryCache)
         {
             _cache = memoryCache;
         }
-
-
         [Route("getCache")]
         [HttpGet]
         [Produces("text/plain")]
@@ -31,7 +20,6 @@ namespace NetCoreApiTokenAuth.Controllers
             Thread.Sleep(5000);
             return DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
         }
-
         [Route("getCache3")]
         [HttpGet]
         [Produces("text/plain")]
@@ -40,9 +28,7 @@ namespace NetCoreApiTokenAuth.Controllers
         {
             Thread.Sleep(5000);
             return DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
-
         }
-
         [Route("getCache2")]
         [HttpGet]
         [Produces("text/plain")]
@@ -53,54 +39,36 @@ namespace NetCoreApiTokenAuth.Controllers
             {
                 _cache.Set("keyim","RAMBO cache");
                 return DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
-
             }
-
             var cashvalue = _cache.Get<string>("keyim");
-
             return cashvalue;
-
         }
-
         [Route("getCache6")]
         [HttpGet]
         [Produces("text/plain")]
         public string GetCache6(int sayi)
         {
-            // Her bir sayý için farklý önbellek anahtarý oluþtur
             string cacheKey = $"getCache6_{sayi}";
-
-            // Önbellekte var mý kontrol et
             if (_cache.TryGetValue(cacheKey, out string cachedValue))
             {
-                // Önbellekte bulunan deðeri kullanabilirsiniz
                 return cachedValue;
             }
             else
             {
-                // Önbellekte bulunmuyorsa, hesaplamayý yap
                 string result = (sayi * 30).ToString();
-
-                // Hesaplanan deðeri önbelleðe ekle
                 _cache.Set(cacheKey, result, new MemoryCacheEntryOptions
                 {
                     AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60) // 60 saniye önbellekte sakla
                 });
-
                 return result;
             }
         }
-
-
         [Route("clearCache")]
         [HttpGet]
         public IActionResult ClearCache()
         {
             _cache.Remove("keyim");
-
             return Ok("Cache cleared");
         }
-
-
     }
 }
